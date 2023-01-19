6511dfce9b38d350a0b6f606fc9f2b6592f2c423
learningman7777 <learningman7777@gmail.com>
2023-01-19 15:36:03 +0900

    Create 백준_10926.java

---
+++ 백준_10926.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class 백준_10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		String n =sc.next();
+		System.out.println(n+"??!");
+	}
+
+}
