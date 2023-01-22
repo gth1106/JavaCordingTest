b010e1d8a1e329306d0fc42133a1db8c14bfd714
learningman7777 <learningman7777@gmail.com>
2023-01-22 23:21:48 +0900

    Create 고양이.java

---
+++ 고양이.java
@@ -0,0 +1,14 @@
+
+public class 고양이 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\\"+"    /\\");
+		System.out.println(" )"+"  ( ')");
+		System.out.println("("+"  /  )");
+		System.out.println(" \\"+"(__)| ");
+		
+	}
+
+}
