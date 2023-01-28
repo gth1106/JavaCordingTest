6100680d37327cc8b2f75524a89abe2948f00224
learningman7777 <learningman7777@gmail.com>
2023-01-28 20:37:33 +0900

    Create 정수입력후출력.java

---
+++ 정수입력후출력.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class 정수입력후출력 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int Size = sc.nextInt();
+		for(int i=0;i<Size;i++) {
+			int A=sc.nextInt();
+			int B=sc.nextInt();
+			System.out.println(A+B);
+		}
+			
+	}
+
+}
