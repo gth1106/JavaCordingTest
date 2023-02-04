82eb4706a1b2ebdfdb288246443434eb1c4c6d0f
learningman7777 <learningman7777@gmail.com>
2023-02-04 17:42:26 +0900

    Create 에이플러스비마이너스5.java

---
+++ 에이플러스비마이너스5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class 에이플러스비마이너스5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while (true )  {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			if(a==0&b==0) {
+				
+				break;
+			}
+		System.out.println(a+b);
+			
+		}
+	}
+
+}
