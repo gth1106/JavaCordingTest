0a07437b35dfd2a9cfc270eb0afa6570bf233fc7
ZzCDKzZ <gth6627@naver.com>
2023-02-06 23:11:22 +0900

    Create 더하기사이클.java

---
+++ 더하기사이클.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class 더하기사이클 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	55	5+5=10 26 -> 2+6=8 68 6+8=14 84 8+4=12 42 4+2=6 
+	50 5+0=5  
+	
+	
+	}
+
+}
