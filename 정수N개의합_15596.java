8638ed2f0df1a2079c296e1550ffbd7d92bb3c2f
ZzCDKzZ <gth6627@naver.com>
2023-02-21 23:20:32 +0900

    Create 정수N개의합_15596.java

---
+++ 정수N개의합_15596.java
@@ -0,0 +1,23 @@
+
+public class 정수N개의합_15596 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 class Test{
+			long sum(int[] a) {
+				long sum=0;
+				
+			for(int i=0;i<a.length;i++) {
+				sum+=a[i];
+				
+			}
+			return sum;
+			
+			}
+			
+		}
+		
+
+	}
+
+}
