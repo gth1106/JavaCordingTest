3faf5cfd609d834a4745119f1825166d7189506c
ZzCDKzZ <gth6627@naver.com>
2023-02-22 19:28:08 +0900

    Create 아스키코드_11654_문자열.java

---
+++ 아스키코드_11654_문자열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class 아스키코드_11654_문자열 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		char a=sc.next().charAt(0);
+		int num=(int) a;
+		System.out.println(num);
+	}
+
+}
