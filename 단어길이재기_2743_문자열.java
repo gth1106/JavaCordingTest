0648cdcd8d09ba42d8417007694a0f220a235d86
ZzCDKzZ <gth6627@naver.com>
2023-02-22 19:28:00 +0900

    Create 단어길이재기_2743_문자열.java

---
+++ 단어길이재기_2743_문자열.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class 단어길이재기_2743_문자열 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String N ;
+		N=sc.next();
+		System.out.println(N.length());
+	}
+
+}
