a8593701cfd224ee14b48bd880d441d5c2db0d94
ZzCDKzZ <gth6627@naver.com>
2023-03-06 23:23:25 +0900

    Create 별찍기연습5.java

---
+++ 별찍기연습5.java
@@ -0,0 +1,17 @@
+
+public class 별찍기연습5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<4;i++) {
+			for(int j=0;j<3-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<2*i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
