24be11ce6f20027405ada304e2d7b20198c26e26
ZzCDKzZ <gth6627@naver.com>
2023-03-08 14:42:32 +0900

    Create 코딩은체육과목입니다_25314_반복문.java

---
+++ 코딩은체육과목입니다_25314_반복문.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class 코딩은체육과목입니다_25314_반복문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		// N 은 4의 배수이다.
+		int N = sc.nextInt();
+		// 4는 long 으로 
+		
+		for(int i=0;i<N/4;i++) {
+			System.out.print("long"+" ");
+		}
+		System.out.print("int");
+		
+	}
+
+}
