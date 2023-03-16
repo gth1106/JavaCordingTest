24656f43cfd3eae9be459c6f9c0027a525ee5cec
ZzCDKzZ <gth6627@naver.com>
2023-03-16 15:15:09 +0900

    Create Light.java

---
+++ Light.java
@@ -0,0 +1,13 @@
+public class Light {
+		public static void main (String arg[]){
+		
+		long lightspeed;
+		long distance;
+
+		lightspeed = 300000;
+		distance = lightspeed * 365 * 24 * 60 * 60;
+		System.out.println("빛이 1년 동안 가는 거리: " +distance + "km.");
+
+
+	}
+}
