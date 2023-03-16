bf75593cf3a2dea47ad99e9c0afb398b7ba98214
ZzCDKzZ <gth6627@naver.com>
2023-03-16 15:14:39 +0900

    Create Add7.java

---
+++ Add7.java
@@ -0,0 +1,20 @@
+public class Add7{
+		public static void main (String arg[]){
+	
+			int i = 26;
+			int oi = 032;
+			int xi =0x1a;
+
+			System.out.println(i);
+			System.out.println(oi);
+			System.out.println(xi);
+			System.out.printf("8진수 출력 %o", i);
+			System.out.println();
+			System.out.printf("16진수 출력 %x",i);
+			System.out.println();
+			System.out.printf("2진수 출력 %s",Integer.toBinaryString(i));
+
+
+
+	}
+}
