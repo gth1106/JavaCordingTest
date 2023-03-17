f4b33b32517c6f88cf845e32207ac69b0c2d0324
ZzCDKzZ <gth6627@naver.com>
2023-03-17 19:14:05 +0900

    Create 클래스_0317_자바입문.java

---
+++ 클래스_0317_자바입문.java
@@ -0,0 +1,22 @@
+package 자바입문;
+
+public class 클래스_0317_자바입문 {
+
+	public class Person{
+		String name;
+		int height;
+		int age;
+		double weight;
+		char gender;
+		boolean married;
+		int studentID;
+		String studentName;
+		int grade;
+		String address;
+		// 위의 멤버 변수를 사용하여 클래스 기능을 구현하는 메소드를 코딩
+		public void showStudentInfo( ) {
+			System.out.println(studentName+" "+address);
+		}
+	}
+	
+}
