4d220336e238290c427274de1ba4d70f3c48b1ea
ZzCDKzZ <gth6627@naver.com>
2023-03-19 22:01:41 +0900

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,10 +1,21 @@
 package classpart;
 
 public class Student {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Student studentAhn =new Student(); //Student 클래스 생성
+		studentAhn.studentName = "안연수";
+/*위와 같이 클래스 내부에 main()함수를 만들면 이 클래스가 프로그램의 시작 클래스가 됩니다.
+ * 
+ */
 	}
 
 }
