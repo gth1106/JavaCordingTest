1797935238cbd3441e602c9aeb6a8d4aa2cf3153
ZzCDKzZ <gth6627@naver.com>
2023-03-23 14:15:08 +0900

    Create java_0323.java

---
+++ java_0323.java
@@ -0,0 +1,19 @@
+package JavaProgramming;
+
+public class java_0323 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=3;
+		int y=4;
+		
+		boolean isTrue;
+		int nextx=x;
+		int nexty=y++;
+		
+		isTrue =(x==nextx)||(y==nexty);
+		System.out.println(isTrue);
+
+	}
+
+}
