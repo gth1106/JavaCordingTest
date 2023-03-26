5aef478d2bae5edde69c67073999594d13bbb161
ZzCDKzZ <gth6627@naver.com>
2023-03-26 15:43:53 +0900

    Create if문_0323_2.java

---
+++ if문_0323_2.java
@@ -0,0 +1,19 @@
+package JavaProgramming;
+
+public class if문_0323_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=99;
+		String size;
+		if(n>=100) {
+			size="large";
+			
+		}
+		else{
+			size="small";
+		}
+		System.out.println(size);
+	}
+
+}
