68fd162c13cd747a184266f5239ddf926ee957f4
ZzCDKzZ <gth6627@naver.com>
2023-04-04 20:26:06 +0900

    Create 함수_문자열함수5.java

---
+++ 함수_문자열함수5.java
@@ -0,0 +1,28 @@
+package backjoon;
+
+public class 함수_문자열함수5 {
+/*
+ *   .equals()
+ *   .length()
+ *   toUppercase
+ *   toLowercase
+ *   
+ *   1. 문자열.indexOf(" ") -> 인덱스 넘버(중복일땐 첫번째 인덱스 넘버)
+ *   
+ *   2. subString -> string 의 일부를 추출
+ *   "Hello".subString(1,3) -> "el"
+ *   
+ *  3.
+ *  replace 문자열의 일부를 찾아서 대체하는 함수
+ *  
+ *  4.
+ *  compareTo 두 문자열의 ASCII 순서 비교 (알파벳 순서비교 함수)
+ *  "a".compareTo("b") -> (-)
+ *  "a".compareTo("a") ->  0
+ *  "b".compareTo("a") -> (+)
+ *  
+ *  5. trim() 공백제거함수
+ * 
+ */
+
+}
