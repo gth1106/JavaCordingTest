3913f11729eab0c432fd4f27222955e39be49f19
ZzCDKzZ <gth6627@naver.com>
2023-04-04 20:43:06 +0900

    Create 재풀이_아스키코드_11654.java

---
+++ 재풀이_아스키코드_11654.java
@@ -0,0 +1,12 @@
+package backjoon;
+import java.util.*;
+public class 재풀이_아스키코드_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		String S=sc.next();
+		//S.compareTo("a");
+		System.out.println(S.compareTo("a")+97);
+	}
+
+}
