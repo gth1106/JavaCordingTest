102c4cddc3f339acf9653cbb767715779b469290
ZzCDKzZ <gth6627@naver.com>
2023-04-06 14:06:38 +0900

    Create test__.java

---
+++ test__.java
@@ -0,0 +1,23 @@
+//12 36
+package JavaProgramming;
+
+public class test__ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		int i=1;
+		
+		n=3;
+		while(i<39) {
+			if(i%8==4){
+				System.out.printf("%d ",n*i);
+			}
+			i++;
+		}
+		System.out.printf("c");
+		return;
+		
+	}
+
+}
