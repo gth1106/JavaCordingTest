61859d8b8b7f1b4eb72c4790018dfd50ae5b4498
ZzCDKzZ <gth6627@naver.com>
2023-04-11 09:56:22 +0900

    Create _0411_별찍기_수업.java

---
+++ _0411_별찍기_수업.java
@@ -0,0 +1,19 @@
+package workspace;
+import java.util.*;
+public class _0411_별찍기_수업 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n;i++) {
+			for(int j=0;j<5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
