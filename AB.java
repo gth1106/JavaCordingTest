54549e8ffb7343a9bf250bf8b1e6af02d9819a8c
ZzCDKzZ <gth6627@naver.com>
2023-05-02 09:10:58 +0900

    Create AB.java

---
+++ AB.java
@@ -0,0 +1,20 @@
+package workspace;
+
+import java.util.*;
+
+public class AB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dan;
+		int i = 0;
+		Scanner sc = new Scanner(System.in);
+		dan = sc.nextInt();
+		while (i < 9) {
+			i++;
+			System.out.println(dan + " * " + i + " = " + dan * i);
+		}
+
+	}
+
+}
