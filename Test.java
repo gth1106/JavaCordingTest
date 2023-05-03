a1aade4143f9f1e906174869537fa8ba347286e2
ZzCDKzZ <gth6627@naver.com>
2023-05-03 09:19:39 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,21 +1,63 @@
-package JavaProgramming;
-import java.util.Scanner;
-public class Test {
-    public static void main(String[] args) {
-          long a = 0, b = 1, c;      
-          int i;
-          Scanner sc =new Scanner(System.in);
-          int count=sc.nextInt();
-          
-          
-          for(i = 1; i <= count; i++) {
-        	 System.out.print(a+" ");
-             c=a+b;
-             a=b;
-             b=c;
-             
-          }
-          
-    }
+package workspace;
+
+public class test {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//		int sum=0;
+//		for(int i=0;i<5;i++) {
+////			System.out.println(i);
+//			for(int j=0;j<10;j++) {
+//				if(i<3||j>4) {
+//					System.out.print(i+" "+j+" ");
+//					sum=sum+i;
+////					i=i+1;
+//					System.out.println(sum);
+//				}
+//				
+//			}
+//				
+//		}
+//		System.out.println(sum);
+//		return;
+//		int inputA=6;
+//		int inputB=7;
+//		int n=6;
+//		int sum=0;
+//		for(int i=0;i<n;i++) {
+//			for(int j=0;j<n-i;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0;j<i;j++) {
+//				System.out.print("*");
+//			}
+//			for(int j=0;j<i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		int dumy=0;
+		for(int i=0;i<300;i++) {
+			for(int j=0;j<100;j++) {
+				for(int k=0;k<5;k++) {
+					dumy++;
+				}
+			}
+		}
+		System.out.println("dumy="+dumy);
+//		int n,i,j,k;
+//		int sum;
+//		int input;
+//		n=40;
+//		input=3;
+//		sum=1;
+//		for(i=0;i<n;i++) {
+//			if()
+//				
+//							
+//		}
+//		System.out.println(input-i);
+	}
+
 }
- 
