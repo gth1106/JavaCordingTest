ea16073beb46ed4785610ed72f3291847d19f3fc
ZzCDKzZ <gth6627@naver.com>
2023-05-07 17:19:18 +0900

    Create asdasdw.java

---
+++ asdasdw.java
@@ -0,0 +1,61 @@
+package workspace;
+import java.util.*;
+public class asdasdw {
+
+			
+		 public static void main(String[] args) {
+		  Scanner s = new Scanner(System.in);
+
+		  int a = s.nextInt();
+		  int b = s.nextInt();
+		  int c = s.nextInt();
+		  int d = s.nextInt();
+		  int total = 500 * a + 100 * b + 50 * c + 10 * d;
+		  if (c > 4) {
+		   System.out.print("A");
+		  } else {
+		   if (b > 2) {
+		    if (a > 2) {
+		     System.out.print("B");
+		    } else {
+		     System.out.print("C");
+		    }
+		    System.out.print("D");
+		   } else {
+		    System.out.print("E");
+		   }
+		   System.out.print("F");
+		  }
+		  if (total > 4) {
+		   System.out.print("A");
+		  } else {
+		   if (b > 2) {
+		    if (a > 2) {
+		     System.out.print("B");
+		    } else {
+		     System.out.print("C");
+		    }
+		    System.out.print("D");
+		   } else {
+		    System.out.print("E");
+		   }
+		   System.out.print("F");
+		  }
+		 
+		  
+	
+		  
+		    int valueA = 10; 
+		    int valueB = 4; 
+		    int result ;
+		    result =  valueA + valueA / valueB;
+		    System.out.print(result);
+		   
+		    
+		   
+
+		  return;
+		 }
+}
+//		키보드 입력내용
+//		1 0 1 2   
