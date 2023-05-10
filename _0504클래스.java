660a10a660d29b1a4280415a707416b3070d4951
ZzCDKzZ <gth6627@naver.com>
2023-05-10 09:10:13 +0900

    Create _0504클래스.java

---
+++ _0504클래스.java
@@ -0,0 +1,33 @@
+package workspace;
+
+public class _0504클래스 {
+
+	public static void main(String[] args) {
+//		String proverb1 = new String("AI Big Data Software Course");
+//		String proverb2 = new String("AI Big Data Software Course");
+//		int i;
+//		char a;
+//		String s1, s2, s3, s4;
+//
+//		if (proverb1 == proverb2) {
+//			System.out.println(10);
+//		} else {
+//			System.out.println(20);
+//		}
+//
+//		return;
+
+		String proverb3 = new String("AI Big Data Software Course");
+		String proverb4 = proverb3;
+		System.out.println(proverb4);
+
+		if (proverb3.equals(proverb4)) {
+			System.out.println(10);
+		} else {
+			System.out.println(20);
+		}
+
+		return;
+
+	}
+}
