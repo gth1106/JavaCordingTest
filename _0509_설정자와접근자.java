e3ad502424e60242be5ab5e5fe020218ed9db847
ZzCDKzZ <gth6627@naver.com>
2023-05-12 13:11:39 +0900

    Create _0509_설정자와접근자.java

---
+++ _0509_설정자와접근자.java
@@ -0,0 +1,37 @@
+//package workspace;
+//
+//public class _0509_설정자와접근자 {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Car object = new Car();
+//		object.speed = 1 ;
+//		
+//		}
+//
+//}
+//
+//class Car {
+//	private String color;
+//	public int speed;
+//	public int gear;
+//	String getColor() {
+//		return color;
+//	}
+//	public void setColor(){
+//		this.color = s;
+//	}
+//	public int getSpeed() {
+//		return speed;
+//	}
+//	public void setSpeed(int speed) {
+//		this.speed = speed;
+//	}
+//	public int getGear() {
+//		return gear;
+//	}
+//	public void setGear(int gear) {
+//		this.gear = gear;
+//	}
+//	
+//}
