5f9c456ced582564b3fb85eed157d3d83392b31b
ZzCDKzZ <gth6627@naver.com>
2023-05-22 23:41:54 +0900

    Create read_아스키코드.java

---
+++ read_아스키코드.java
@@ -0,0 +1,13 @@
+package backjoon재풀이;
+import java.io.*;
+public class read_아스키코드 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int S = br.read();
+		System.out.println(S);
+		
+	}
+
+}
