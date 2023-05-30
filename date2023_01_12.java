49e80dc8375d877cf14285082e7b2bdf5f893cdb
ZzCDKzZ <gth6627@naver.com>
2023-05-30 12:36:05 +0900

    Create date2023_01_12.java

---
+++ date2023_01_12.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class date2023_01_12 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int y=sc.nextInt();
+		System.out.println(y-543);
+	
+
+		
+		
+	}
+
+}
