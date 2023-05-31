2c8fbb6d8c1224527866f9744b736e38abd23da3
ZzCDKzZ <gth6627@naver.com>
2023-05-31 14:11:28 +0900

    Create Java_0330quiz.java

---
+++ Java_0330quiz.java
@@ -0,0 +1,96 @@
+package JavaProgramming;
+
+public class Java_0330quiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int x,y,r;
+//		x=12;
+//		y=124;
+//		while(y!=0) {
+//			r=x%y;
+//			x=y;
+//			y=r;
+//		}
+//		System.out.println(x);
+//		int n=3;
+//		int i=1;
+//		while(i<9) {
+//			if(i==9) {
+//			System.out.printf("%d*%d= %d \n",n,i,n*i);
+//		}
+//		i++;
+//	}
+//		System.out.printf();
+//		int a,b,x,y,r;
+//		a=48;b=12;
+//		if(a>b) {
+//			x=a;y=b;
+//		}
+//		else {
+//			x=b;
+//			y=a;
+//		}
+//		int i=0;
+//		int gcd =1;
+//		while (i<b) {
+//			if((x%(i+1)==0)&&(y%(i+1)==0)) {
+//				gcd=(i+1);
+//			}
+//			i=i+1;
+//		}
+//		System.out.println(a+"와 "+b+gcd);
+//		int i=0;
+//		int sum=0;
+//		while(i<15) {
+//			sum+=i;
+//			i++;
+//		}
+//		System.out.println("a"+i+"b"+sum);
+//		
+//		int i=0;
+//		int sum=0;
+//		while(i<3) {
+//			i=i+1;
+//			sum=sum+i;
+//			System.out.print("합은 "+sum);
+//		}
+//		int i=0;int sum=2;
+//		while(i<5) {
+//			sum=sum+(i+1);
+//			i=i+1;
+//		}
+//			System.out.println("while sum ="+i);
+//		int n;
+//		int i=0;
+//		int sum=0;
+//		n=3;
+//		while(i<9) {
+//			if((i+1)%2==1) {
+//				sum=sum+n*(i+1);
+//			}
+//			i++;
+//			
+//		}
+//		System.out.printf("%d*%d= %d \n",n,i,n*i);
+//		int i=0;
+//		int sum=0;
+//		int k;
+//		while(i<30) {
+//			k=i+1;
+//			if(k%3==2) {
+//				sum=sum+k;
+//			}
+//			i=i+1;
+//		}
+//		System.out.println(sum);
+		
+		
+		}
+		
+		
+		
+	}
+	
+
+
