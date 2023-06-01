23babdb4760cdf28cdc96b49c9a8de7c1b5d5d37
ZzCDKzZ <gth6627@naver.com>
2023-06-01 10:11:48 +0900

    Create qweqweqasdasd.java

---
+++ qweqweqasdasd.java
@@ -0,0 +1,22 @@
+package workspace;
+
+public class qweqweqasdasd {
+	
+		   public static void main(String[] args) {
+		      String proverb1 = new String("8");
+		      String proverb2 = new String("37.4");
+		      int a, b, c, d;
+		      double e, f;
+		      String s1, s2, s3, s4;
+
+		      a = b = c = d = 43;
+		      f = a / 5.0;
+		      s1 = "1" + f;
+		      e = c + Double.parseDouble(s1);
+		      s4 = proverb1 + e;
+
+		      System.out.println(s4);
+
+		      return;
+		   }
+		}
