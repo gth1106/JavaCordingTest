fcc2d6e82b24e9e17feb0b0a40d59ab502095fae
ZzCDKzZ <gth6627@naver.com>
2023-06-02 19:01:08 +0900

    Create 실패_백준바구니뒤집기재풀이.java

---
+++ 실패_백준바구니뒤집기재풀이.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class 실패_백준바구니뒤집기재풀이 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		
+		int arr[] = new int[N];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=i+1;
+		}
+		for(int i=0;i<M;i++) {
+			int A=sc.nextInt();
+			int B=sc.nextInt();
+			
+		}
+		
+		
+		
+	}
+
+}
