c897778b40aaa7fc706e2c4d3bbeb8c604916ca6
ZzCDKzZ <gth6627@naver.com>
2023-06-05 10:09:56 +0900

    Create test2___.java

---
+++ test2___.java
@@ -0,0 +1,12 @@
+package JavaProgramming;
+import java.util.*;
+public class test2___ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
