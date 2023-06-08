c006f3074e309ec2b6af0e110f2ca8638b7d8796
ZzCDKzZ <gth6627@naver.com>
2023-06-08 15:15:53 +0900

    Create 꼬마정민.java

---
+++ 꼬마정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class 꼬마정민 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long A=sc.nextLong();
+		long B=sc.nextLong();
+		long C=sc.nextLong();
+		System.out.println(A+B+C);
+	}
+
+}
