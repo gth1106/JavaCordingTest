374fbadbd9af67c5de250a03efe327d7c5ae3d1b
ZzCDKzZ <gth6627@naver.com>
2023-06-08 15:16:01 +0900

    Create 단어개수세기.java

---
+++ 단어개수세기.java
@@ -0,0 +1,13 @@
+package backjoon재풀이;
+import java.io.*;
+public class 단어개수세기 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String []st = br.readLine().split(" ");
+		
+		System.out.println(st.length);
+	}
+
+}
