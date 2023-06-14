17b558684b3f3451aa81721c7113fb58796076da
ZzCDKzZ <gth6627@naver.com>
2023-06-14 17:53:34 +0900

    Create 백준_문자와문자열_27866.java

---
+++ 백준_문자와문자열_27866.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class 백준_문자와문자열_27866 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String S=sc.next();
+		int Size = sc.nextInt();
+		int N[]=new int[S.length()];
+		for(int i=0;i<S.length();i++) {
+			S=N[i];
+			
+		}
+	}
+
+}
