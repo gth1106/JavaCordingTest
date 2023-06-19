0bd20b669c014f1cf51bff813b8640f0064adb6c
ZzCDKzZ <gth6627@naver.com>
2023-06-20 02:34:36 +0900

    Create 숫자의합.java

---
+++ 숫자의합.java
@@ -0,0 +1,12 @@
+package backjoon재풀이;
+import java.io.*;
+public class 숫자의합 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int s=br.read();
+		System.out.println(s);
+	}
+
+}
