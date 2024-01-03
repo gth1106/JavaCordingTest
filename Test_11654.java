7945ad39d67cc81b2a0758e48319ecd24f6f4f0d
taehyun <gth6627@naver.com>
2024-01-03 20:58:24 +0900

    Create Test_11654.java

---
+++ Test_11654.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.read());
+    }
+}
